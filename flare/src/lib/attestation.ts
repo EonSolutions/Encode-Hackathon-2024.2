685b874d6ad4e8d11d6db659f1c3e35226ba3413
Cy4 <veschi@gmail.com>
2024-10-26 11:21:24 +0100

    attestation.ts

---
+++ flare/src/lib/attestation.ts
@@ -0,0 +1,39 @@
+const ATTESTATION_URL = "http://localhost:8000"
+
+function toHex(data: string): string {
+  var result = "";
+  for (var i = 0; i < data.length; i++) {
+    result += data.charCodeAt(i).toString(16);
+  }
+  return "0x" + result.padEnd(64, "0");
+}
+
+interface AttestationRequest {
+  abiEncodedRequest: string;
+  status: string;
+}
+
+async function prepareAttestationRequest(
+  attestationType: string,
+  sourceId: string,
+  requestBody: any,
+): Promise<AttestationRequest> {
+  const response = await fetch(
+    `${ATTESTATION_URL}/${attestationType}/prepareRequest`,
+    {
+      method: "POST",
+      headers: {
+        // "X-API-KEY": ATTESTATION_API_KEY as string,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        attestationType: toHex(attestationType),
+        sourceId: toHex(sourceId),
+        requestBody: requestBody,
+      }),
+    },
+  );
+  const data = await response.json();
+  return data;
+}
+export { prepareAttestationRequest };
