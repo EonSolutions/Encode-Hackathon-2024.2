acf797f82db1265ab6d6853aec05d6c2cb70adc9
Cy4 <veschi@gmail.com>
2024-10-26 12:09:34 +0100

    Firebase connection

--- flare/src/app/(routes)/api/db/put/route.ts
+++ flare/src/app/(routes)/api/db/put/route.ts
@@ -3,9 +3,7 @@ import {db} from "@/lib/firebaseConfig";
 
 export async function POST(req: Request) {
     const body = await req.json();
-    console.log(body);
     const encryptedFeedback = body.encryptedFeedback;
-    console.log(encryptedFeedback);
     const docRef = await addDoc(collection(db, "feedbacks"), {
         encryptedFeedback: encryptedFeedback,
         timestamp: new Date(),
